e6a04c8b81f31c1c22eefa625a2e05d54d01cd67
Elizaveta <al5xeevaliz@yandex.ru>
2021-12-26 22:06:08 +0300

    save example

--- MyHomeWork/Example/Program.cs
+++ MyHomeWork/Example/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из чисел, больших 8. Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом.
+void FillArray(int[] array)
+{
+    int size = array.Length;
+    int index = 0;
+    while (index < size)
+    {
+        array[index] = new Random().Next(1, 20);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+    int position = 0;
+    while (position < size)
+    {
+        Console.Write($"{array[position]} ");
+        position++;
+    }
+}
+
+void ChangeArray(int[] array)
+{
+    int N = 8;
+    int[] mas2 = new int[array.Length];
+    for (int i = 0; i < mas2.Length; i++)
+    {
+        mas2[i] = array[i];
+        if (mas2[i] > N)
+        {
+            Console.Write($"{mas2[i]} ");
+        }
+    }
+}
+
+int[] array = new int[5];
+FillArray(array);
+PrintArray(array);
+Console.Write($"-> ");
+ChangeArray(array);
